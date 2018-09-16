ad9e3537cba57b779563bb18df51ae983bef7b84
blackbelts-egypt <mabdelwahed120468@gmail.com>
2018-09-16 17:09:39 +0200

    allocation submission

--- models/Allocation.py
+++ models/Allocation.py
@@ -12,3 +12,16 @@ class Allocation(models.Model):
     allocation_total=fields.Float('Allocation Total')
     allocation_invested = fields.Float('Allocation Invested')
     allocation_O_S = fields.Float('Allocation O/S')
+    allocation_line=fields.One2many('allocation.lines','allocation_id')
+
+
+
+class Allocation_lines(models.Model):
+    _name='allocation.lines'
+
+    sub=fields.Many2one('ppf.subscription',domain="[('state','=','paid')]",string='Subscribtion')
+    sub_date = fields.Date(related='sub.date_invoice',string='Subscribtion Date')
+    sub_total_ammount=fields.Float(related='sub.totalamount',readonly=True,force_save=True)
+    allocated=fields.Float('Allocated')
+    out_standing=fields.Float('O/S')
+    allocation_id=fields.Many2one('allocation')
