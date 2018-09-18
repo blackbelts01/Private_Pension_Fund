bd4cc177b16d84f3c4b4d8827106bc0874c25737
blackbelts-egypt <mabdelwahed120468@gmail.com>
2018-09-18 17:08:17 +0200

    investments

--- models/Allocation.py
+++ models/Allocation.py
@@ -13,15 +13,26 @@ class Allocation(models.Model):
     allocation_invested = fields.Float('Allocation Invested')
     allocation_O_S = fields.Float('Allocation O/S')
     allocation_line=fields.One2many('allocation.lines','allocation_id')
+    allocation_line_invest = fields.One2many('investment.lines', 'allocation_id_invest')
 
 
 
 class Allocation_lines(models.Model):
     _name='allocation.lines'
 
-    sub=fields.Many2one('account.invoice',domain="[('state','=','paid')]",string='Subscribtion')
+    sub=fields.Many2one('account.invoice',domain="[('state','=','paid'),('type','=','in_invoice')]",string='Subscribtion')
     sub_date = fields.Date(related='sub.date_invoice',string='Subscribtion Date')
     sub_total_ammount=fields.Float(related='sub.totalamount',readonly=True,force_save=True)
     allocated=fields.Float('Allocated')
     out_standing=fields.Float('O/S')
     allocation_id=fields.Many2one('allocation')
+
+class Investment_lines(models.Model):
+    _name='investment.lines'
+    invest=fields.Many2one('account.invoice',domain="[('state','=','paid'),('type','=','in_invoice')]",string='investment')
+    allocation_id_invest = fields.Many2one('allocation')
+    # sub_date = fields.Date(related='sub.date_inv
+    # sub_total_ammount=fields.Float(related='sub.
+    # allocated=fields.Float('Allocated')
+    # out_standing=fields.Float('O/S')
+    # allocation_id=fields.Many2one('allocation')
