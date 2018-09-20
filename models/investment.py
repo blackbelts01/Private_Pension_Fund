35e499bcf58387e482880c068b663d10965e0c43
Mohamed Saber <mohamedabosaber94@gmail.com>
2018-09-20 10:43:24 +0200

    inv tree

--- models/investment.py
+++ models/investment.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 class ppfInvestment(models.Model):
     _inherit = 'account.invoice'
 
-    allocation_id = fields.Many2one('allocation', string="Allocation")
+    allocation_id = fields.Many2many('allocation', string="Allocation")
     totalamount=fields.Float(store=True, readonly=True,string='Total Amount',compute='_onchange_sum_amount')
 
     @api.one
