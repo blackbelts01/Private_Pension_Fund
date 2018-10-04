115d9cdd83e6f4b7d9869a7656bf3f050e52496b
Mohamed Saber <mohamedabosaber94@gmail.com>
2018-10-04 16:17:09 +0200

    total amount

--- models/subscriptions.py
+++ models/subscriptions.py
@@ -9,16 +9,22 @@ class ppfInvoice(models.Model):
     allocated=fields.Float('Allocated')
     o_s=fields.Float('O/S')
     ref_id = fields.Many2one('account.invoice', string="Invoice")
-    totalamount=fields.Float(store=True, readonly=True,string='Total Amount',compute='_onchange_sum_amount')
+    totalamount=fields.Float(store=True, readonly=True,string='Total Amount',compute='_onchange_totalamount')
 
     @api.one
-    @api.depends('amount_total')
-    def _onchange_sum_amount(self):
-        self.totalamount = self.amount_total
+    @api.depends('invoice_line_ids')
+    def _onchange_totalamount(self):
+        if self.invoice_line_ids:
+            self.totalamount=0.0
+            for record in self.invoice_line_ids:
+                self.totalamount += record.total
+
 
     @api.onchange('allocated', 'totalamount')
     def _onchange_sum(self):
         self.o_s = self.totalamount - self.allocated
+
+
 
 
 
@@ -32,11 +38,17 @@ class invoiceLine(models.Model):
     member_id = fields.Char(related='member_name.member_id',string='Member ID',store=True,readonly=True)
     member_name = fields.Many2one('res.partner',string='Member Name')
     salary = fields.Float(string='Salary')
-    perc_salary = fields.Float(' % of Salary')
+    perc_salary = fields.Integer(' % of Salary')
     own = fields.Float('Own')
     company = fields.Float('Company')
     booster = fields.Float('Booster')
     side = fields.Float('Side')
+    total=fields.Float('Total',store=True, readonly=True,compute='_compute_total')
+
+    @api.one
+    @api.depends('salary','perc_salary','own','company','booster','side')
+    def _compute_total(self):
+        self.total = (self.salary * (self.perc_salary/100))+self.own+self.company+self.booster+self.side
 
 
 
